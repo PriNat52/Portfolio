2a17831d83483c5b288ed8e3c6876d90cd16c3ec
PriNat52 <75464396+PriNat52@users.noreply.github.com>
2020-12-04 17:59:43 -0500

    finalproject_trial

---
+++ form/form.js
@@ -0,0 +1,39 @@
+
+function Submit(){
+  let firstName = document.getElementById("firstName").value;
+  let firstNameError = document.getElementById("firstNameError");
+  let lastNameError = document.getElementById("lastNameError");
+  let subjectNameError = document.getElementById("subjectrNameError");
+  let lastName = document.getElementById("lastName").value;
+  let subject = document.getElementById("subject").value;
+  let exp = /[A-Za-z ]{2,30}$/gi;
+  //Requirement 2: Validate both firstName and lastName
+  if (firstName.length < 2 | !firstName.match(exp)){
+    errorf();
+    firstNameError.innerHTML = firstNameError.textContent;
+    return false;
+  }else if(lastName.length < 2 | !lastName.match(exp)){
+    errorl();
+    lastNameError.innerHTML = lastNameError.textContent;
+    return false;
+  }else if(subject.length < 2 | !subject.match(exp)){
+    errorl();
+    subjectNameError.innerHTML = subjectNameError.textContent;
+    return false;
+}
+
+function move(e){
+  document.getElementById("firstName").style.backgroundColor= "lightblue";
+  document.getElementById("lastName").style.backgroundColor= "lightblue";
+  document.getElementById("subject").style.backgroundColor= "lightblue";
+}
+
+function errorf(e){
+  document.getElementById("firstName").style.backgroundColor= "red";
+}
+function errorl(e){
+  document.getElementById("lastName").style.backgroundColor= "red";
+}
+function error(e){
+  document.getElementById("subject").style.backgroundColor= "red";
+}
